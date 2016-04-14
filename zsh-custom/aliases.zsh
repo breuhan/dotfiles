e969a7f1f85d2d3a5dc66b34664d08b45071557e
Andy Breuhan <andybreuhan@andybreuhan.de>
2016-04-14 09:50:41 +0200

    added recursive git pull alias

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -25,19 +25,20 @@ export PATH=$PATH:/usr/local/texlive/2012basic/bin/x86_64-darwin
 export PATH=$PATH:$HOME/Dropbox/Programmierung/bin
 export PATH=$PATH:$HOME/bin
 
-export PATH=/Developer/NVIDIA/CUDA-5.5/bin:$PATH
-export DYLD_LIBRARY_PATH=/Developer/NVIDIA/CUDA-5.5/lib:$DYLD_LIBRARY_PATH
+export PATH=/Developer/NVIDIA/CUDA-7.5/bin:$PATH
+export DYLD_LIBRARY_PATH=/Developer/NVIDIA/CUDA-7.5/lib/:$DYLD_LIBRARY_PATH
 
 alias itunespush="rsync -avz --delete $HOME/Music/iTunes/ avoton.home.breuhan.de:'/export/tank/Audio/iTunes/'"
 alias itunespull="rsync -avz --delete avoton.home.breuhan.de:'/export/tank/Audio/iTunes/' $HOME/Music/iTunes/"
 alias iphotopush="rsync -avz --progress --delete --iconv=UTF-8-MAC,UTF-8 $HOME/Pictures/ avoton.home.breuhan.de:'/export/volatile/Andy/Bilder/'"
 alias iphotopull="rsync -avz --delete avoton.home.breuhan.de:'/export/volatile/Andy/Bilder/' $HOME/Pictures/"
-alias hidrivepush="rsync -avzhPy --inplace --delete --delete-excluded --exclude-from=/MiniDrive/Dateien/.rsync_exclude --iconv=UTF-8-MAC,UTF-8 /MiniDrive/Dateien/ cylonbrain@rsync.hidrive.strato.com:'/users/cylonbrain/Dateien/'"
-alias hidrivepull="rsync -av --progress --delete --iconv=UTF-8-MAC,UTF-8 cylonbrain@rsync.hidrive.strato.com:'/users/cylonbrain/Dateien/' /MiniDrive/Dateien/"
-alias dateienpush="rsync -avzhPy --inplace --delete --delete-excluded --exclude-from=/MiniDrive/Dateien/.rsync_exclude --iconv=UTF-8-MAC,UTF-8 /MiniDrive/Dateien/ avoton.home.breuhan.de:'/export/volatile/Andy/Dateien/'"
+alias hidrivepush="rsync -avzhPy --inplace --delete --delete-excluded --exclude-from=/Volumes/MiniDrive/Dateien/.rsync_exclude --iconv=UTF-8-MAC,UTF-8 /Volumes/MiniDrive/Dateien/ cylonbrain@rsync.hidrive.strato.com:'/users/cylonbrain/Dateien/'"
+alias hidrivepull="rsync -av --progress --delete --iconv=UTF-8-MAC,UTF-8 cylonbrain@rsync.hidrive.strato.com:'/users/cylonbrain/Dateien/' /Volumes/MiniDrive/Dateien/"
+alias dateienpull="rsync -avzhPy --inplace --delete --delete-excluded --exclude-from=/Volumes/MiniDrive/Dateien/.rsync_exclude  avoton.home.breuhan.de:'/export/volatile/Andy/Dateien/'" /Volumes/MiniDrive/Dateien/
 
 alias hawkthorne="cd Source/hawkthorne-journey/ && git pull && make && open build/hawkthorne.love && cd -"
 alias sublime="open -a \"Sublime Text 2\""
 
 alias gitgogo="git commit -m \"changes from $(uname -n) on $(date)\""
 alias defaultpermissions="find . -type d -print0 | xargs -0 chmod 0755 && find . -type f -print0 | xargs -0 chmod 0644"
+alias git-recursive-pull='find . -type d -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull" \;'
